9e4db4a9a4df1ff4a39e0a2eb2b25458d0466efd
Biju Philip Chacko <biju.chacko@gmail.com>
2017-10-03 12:20:24 +0530

    First step towards using models for domain objects

--- boson/tasks.py
+++ boson/tasks.py
@@ -16,22 +16,7 @@ from sqlalchemy.sql import table, column, select, update, insert
 
 from pprint import pprint as pp
 
-class DB(object):
-    """ Singleton Encapsulation of the database connection """
-    __instance = None
-
-    def __new__(cls):
-        if DB.__instance is None:
-            DB.__instance = object.__new__(cls)
-            with open('database.yml', 'r') as f:
-                db_config = yaml.load(f)
-            env = os.getenv('BOSON_ENV', 'development')
-            connection_string = db_config[env]
-            logging.debug("connecting to " + connection_string)
-            DB.__instance.engine = create_engine(connection_string)
-            DB.__instance.connection = DB.__instance.engine.connect()
-        
-        return DB.__instance
+from boson.database import DB
 
 class CreateList(object):
     """ Understands how to instantiate a list """
